0e72cb6ecaeb5f5042caa8d21a1c3488b6606b02
Aidensaid <ai.loughlin@gmail.com>
2020-07-22 17:13:37 +1000

    Wired up event handler to state

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,17 +1,24 @@
 import React from 'react';
 
 
+
 class SelectableTable extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      id: ''
+      15: { color: 'green', isSelected: false },
+      120: { color: 'red', isSelected: false },
+      33: { color: 'blue', isSelected: false },
+      85: { color: 'red', isSelected: false },
+      1: { color: 'green', isSelected: false }
     }
     this.handleSelect = this.handleSelect.bind()
   }
 
   handleSelect = (event) => {
     console.log(event.target.name)
+    this.setState({ [event.target.name]: { isSelected: true } })
+    console.log(this.state)
   }
 
   render() {
@@ -19,13 +26,13 @@ class SelectableTable extends React.Component {
       <div>
         <table className="ui collapsible table">
           <th>Name
-           {this.props.users.map((user) => <td>{user.name}</td>)}
+          {this.props.users.map((user) => <tr>{user.name}</tr>)}
           </th>
           <th>Favourite color
-            {this.props.users.map((user) => <td>{user.favoriteColor}</td>)}
+          {this.props.users.map((user) => <tr>{user.favoriteColor}</tr>)}
           </th>
           <th>Selected?
-            {this.props.users.map((user) => <input onClick={this.handleSelect} name={user.id} type="checkbox"></input>)}
+          {this.props.users.map((user) => <tr><input onClick={this.handleSelect} name={user.id} type="checkbox"></input></tr>)}
           </th>
         </table>
         <div className="colors">
