e6f713dd62cbde1a3c0fb046087df8f7aab51552
Aidensaid <ai.loughlin@gmail.com>
2020-07-28 15:14:05 +1000

    Created getSuggestions function

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,43 +1,33 @@
 import React from 'react'
 
-// const SearchBar = () => {
-//   return <div className="ui segment">
-//     <form className="ui form">
-//       <div className="field">
-//         <label>Search</label>
-//         <input type="text" onFocus={() => this.onFocus} />
-//       </div>
-//     </form>
-//   </div>
-// }
-
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { showOptions: false }
-    // this.onFocus = this.onFocus.bind()
+    this.state = {
+      showOptions: false,
+      searchTerm: ''
+    }
   }
 
-  // componentDidMount() {
-  //   this.onFocus()
-  // }
-
-  // onFocus = () => {
-  //   this.setState({ showOptions: true })
-  // }
+  getSuggestions = () => {
+    this.array = this.props.options.map(color => color.value)
+    this.filteredArray = this.array.filter(color => color[0] === this.state.searchTerm).slice(0, 5)
+    console.log(this.filteredArray)
+  }
 
   render() {
+    this.getSuggestions()
+    // console.log(this.state.searchTerm)
     return (
       <>
         <div className="ui segment">
           <form className="ui form">
             <div className="field">
               <label>Search</label>
-              <input type="text" onFocus={() => this.setState({ showOptions: true })} onBlur={() => this.setState({ showOptions: false })} />
+              <input type="text" onChange={(event) => this.setState({ searchTerm: event.target.value })} onFocus={() => this.setState({ showOptions: true })} onBlur={() => this.setState({ showOptions: false })} />
             </div>
           </form>
         </div>
-        {/* <div>{<SearchBar onFocus={this.onFocus} />}</div> */}
         <div>{(this.state.showOptions) ? this.props.options.map((option) => <li>{option.value}</li>) : null}</div>
       </>
     )
