8576b8cdd3fb411846624fa814779a3ef1de84ca
Aidensaid <ai.loughlin@gmail.com>
2020-07-28 14:33:03 +1000

    Created event handlers to toggle visibility of options list

--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,14 +1,45 @@
 import React from 'react'
 
+// const SearchBar = () => {
+//   return <div className="ui segment">
+//     <form className="ui form">
+//       <div className="field">
+//         <label>Search</label>
+//         <input type="text" onFocus={() => this.onFocus} />
+//       </div>
+//     </form>
+//   </div>
+// }
+
 class AutoComplete extends React.Component {
   constructor(props) {
     super(props)
-    this.state = { color: 'red' }
+    this.state = { showOptions: false }
+    // this.onFocus = this.onFocus.bind()
   }
+
+  // componentDidMount() {
+  //   this.onFocus()
+  // }
+
+  // onFocus = () => {
+  //   this.setState({ showOptions: true })
+  // }
 
   render() {
     return (
-      <div>{this.props.options.map((option) => <li>{option.value}</li>)}</div>
+      <>
+        <div className="ui segment">
+          <form className="ui form">
+            <div className="field">
+              <label>Search</label>
+              <input type="text" onFocus={() => this.setState({ showOptions: true })} onBlur={() => this.setState({ showOptions: false })} />
+            </div>
+          </form>
+        </div>
+        {/* <div>{<SearchBar onFocus={this.onFocus} />}</div> */}
+        <div>{(this.state.showOptions) ? this.props.options.map((option) => <li>{option.value}</li>) : null}</div>
+      </>
     )
   }
 }
