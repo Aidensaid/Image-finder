867981380ba22f484e8431f4ce6d7c6af09262df
Aidensaid <ai.loughlin@gmail.com>
2020-07-31 10:38:22 +1000

    Added logic to toggle next question button

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -2,16 +2,27 @@ import React, { useState } from 'react'
 
 const Questions = (props) => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [userSelection, setUserSelection] = useState(null)
   const handleNextQuestion = () => {
     setCurrentQuestion(currentQuestion + 1)
     console.log(currentQuestion)
   }
+  const handlePrevQuestion = () => {
+    setCurrentQuestion(currentQuestion - 1)
+    console.log(currentQuestion)
+  }
+  const handleSelection = (event) => {
+    setUserSelection(event.target.name)
+    console.log(event.target.checked)
+  }
   return (
     <>
       <div>{props.quiz[currentQuestion].title}</div>
-      <div>{props.quiz[currentQuestion].options.map((option) => <li>{option}<input style={{ marginLeft: "30px;" }} type="radio" value={option}></input></li>)}</div>
-      <button>Submit answer</button>
-      <button onClick={handleNextQuestion}>Next question</button>
+      <form>{props.quiz[currentQuestion].options.map((option) => <li>{option}<input name={option} onClick={handleSelection} checked={userSelection === option} style={{ marginLeft: "30px;" }} type="radio" value={option}></input></li>)}</form>
+      {/* <button>Submit answers</button> */}
+      <button onClick={handlePrevQuestion}>Last question</button>
+      <button disabled={userSelection === null} onClick={handleNextQuestion}>Next question</button>
+
     </>
   )
 }
