864c18fbf710f488945a74aa093341f1b37cad01
Aidensaid <ai.loughlin@gmail.com>
2020-07-31 11:59:47 +1000

    Stored user answers in state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -3,6 +3,8 @@ import React, { useState } from 'react'
 const Questions = (props) => {
   const [currentQuestion, setCurrentQuestion] = useState(0)
   const [userSelection, setUserSelection] = useState(null)
+  const [firstQuestionAnswer, setFirstQuestionAnswer] = useState(null)
+  const [secondQuestionAnswer, setSecondQuestionAnswer] = useState(null)
   const handleNextQuestion = () => {
     setCurrentQuestion(currentQuestion + 1)
     console.log(currentQuestion)
@@ -13,13 +15,16 @@ const Questions = (props) => {
   }
   const handleSelection = (event) => {
     setUserSelection(event.target.name)
-    console.log(event.target.checked)
+    setFirstQuestionAnswer(currentQuestion === 0 ? event.target.value : firstQuestionAnswer)
+    setSecondQuestionAnswer(currentQuestion === 1 ? event.target.value : secondQuestionAnswer)
+    console.log('usrSlct state', userSelection)
+    console.log('firstQuestionAnswer', firstQuestionAnswer)
+    console.log('secondQuestionAnswer', secondQuestionAnswer)
   }
   return (
     <>
       <div>{props.quiz[currentQuestion].title}</div>
-      <form>{props.quiz[currentQuestion].options.map((option) => <li>{option}<input name={option} onClick={handleSelection} checked={userSelection === option} style={{ marginLeft: "30px;" }} type="radio" value={option}></input></li>)}</form>
-      {/* <button>Submit answers</button> */}
+      <form>{props.quiz[currentQuestion].options.map((option, idx) => <li>{option}<input name={option} onClick={handleSelection} checked={userSelection === option} style={{ marginLeft: "30px;" }} type="radio" value={idx}></input></li>)}</form>
       <button onClick={handlePrevQuestion}>Last question</button>
       <button disabled={userSelection === null} onClick={handleNextQuestion}>Next question</button>
 
