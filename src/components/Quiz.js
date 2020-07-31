c122f1403d299ffe0e8a01444fe231692d0358dd
Aidensaid <ai.loughlin@gmail.com>
2020-08-01 07:14:28 +1000

    Solved quiz answer bug by decomposing problem into little bites ;)

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,33 +1,49 @@
 import React, { useState } from 'react'
 
+
 const Questions = (props) => {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [userSelection, setUserSelection] = useState(null)
-  const [firstQuestionAnswer, setFirstQuestionAnswer] = useState(null)
-  const [secondQuestionAnswer, setSecondQuestionAnswer] = useState(null)
-  const handleNextQuestion = () => {
-    setCurrentQuestion(currentQuestion + 1)
-    console.log(currentQuestion)
+  // const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [userSelection, setUserSelection] = useState([])
+  // const [firstQuestionAnswer, setFirstQuestionAnswer] = useState(null)
+  // const [secondQuestionAnswer, setSecondQuestionAnswer] = useState(null)
+  const [results, setResults] = useState(0)
+  const [submitted, setSubmitted] = useState(false)
+
+  // const handleNextQuestion = () => {
+  //   setCurrentQuestion(currentQuestion + 1)
+  //   console.log('user selection', userSelection)
+  //   setFirstQuestionAnswer(userSelection)
+  // }
+  // const handlePrevQuestion = () => {
+  //   setCurrentQuestion(currentQuestion - 1)
+  // }
+
+  // const handleSelection = (event) => {
+  //   setUserSelection(event.target.name)
+  //   console.log('user selection', userSelection)
+  // }
+
+  const returnResults = () => {
+
+    console.log(userSelection)
+    const answer = props.quiz[0].answer
+    if (userSelection === answer) {
+      setResults(+ 1)
+      console.log('correct')
+    }
+    // let score = 0
+    // setResults(score)
+    setSubmitted(true)
   }
-  const handlePrevQuestion = () => {
-    setCurrentQuestion(currentQuestion - 1)
-    console.log(currentQuestion)
-  }
-  const handleSelection = (event) => {
-    setUserSelection(event.target.name)
-    setFirstQuestionAnswer(currentQuestion === 0 ? event.target.value : firstQuestionAnswer)
-    setSecondQuestionAnswer(currentQuestion === 1 ? event.target.value : secondQuestionAnswer)
-    console.log('usrSlct state', userSelection)
-    console.log('firstQuestionAnswer', firstQuestionAnswer)
-    console.log('secondQuestionAnswer', secondQuestionAnswer)
-  }
+
   return (
     <>
-      <div>{props.quiz[currentQuestion].title}</div>
-      <form>{props.quiz[currentQuestion].options.map((option, idx) => <li>{option}<input name={option} onClick={handleSelection} checked={userSelection === option} style={{ marginLeft: "30px;" }} type="radio" value={idx}></input></li>)}</form>
-      <button onClick={handlePrevQuestion}>Last question</button>
-      <button disabled={userSelection === null} onClick={handleNextQuestion}>Next question</button>
-
+      {!submitted && <div>{props.quiz[0].title}</div>}
+      {!submitted && <form>{props.quiz[0].options.map((option, idx) => <li>{option}<input name={option} onClick={() => setUserSelection(idx)} checked={userSelection === idx} style={{ marginLeft: "30px;" }} type="radio" value={idx}></input></li>)}</form>}
+      {/* <button onClick={handlePrevQuestion}>Last question</button> */}
+      <button disabled={userSelection === null}>Next question</button>
+      <button onClick={returnResults}>submit</button>
+      {submitted && <div>`You scored {results} out of 1`</div>}
     </>
   )
 }
