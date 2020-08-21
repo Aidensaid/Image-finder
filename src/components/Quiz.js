0aefa9fda701b5b10be88a5cf188cde9c1b4db75
Aidensaid <ai.loughlin@gmail.com>
2020-08-21 15:22:02 +1000

    Added conditionals so users can't go to prev/next question on first last q

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,51 +1,55 @@
 import React, { useState } from 'react'
 
-
 const Questions = (props) => {
-  // const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [userSelection, setUserSelection] = useState([])
-  // const [firstQuestionAnswer, setFirstQuestionAnswer] = useState(null)
-  // const [secondQuestionAnswer, setSecondQuestionAnswer] = useState(null)
+  const [currentQuestion, setCurrentQuestion] = useState(0)
+  const [userSelection, setUserSelection] = useState()
+  const [radioChecked, setRadioChecked] = useState(0)
   const [results, setResults] = useState(0)
   const [submitted, setSubmitted] = useState(false)
 
-  // const handleNextQuestion = () => {
-  //   setCurrentQuestion(currentQuestion + 1)
-  //   console.log('user selection', userSelection)
-  //   setFirstQuestionAnswer(userSelection)
-  // }
-  // const handlePrevQuestion = () => {
-  //   setCurrentQuestion(currentQuestion - 1)
-  // }
-
-  // const handleSelection = (event) => {
-  //   setUserSelection(event.target.name)
-  //   console.log('user selection', userSelection)
-  // }
-
-  const returnResults = () => {
-
-    console.log(userSelection)
-    const answer = props.quiz[0].answer
-    if (userSelection === answer) {
-      setResults(+ 1)
-      console.log('correct')
+  const handleNextQuestion = () => {
+    if (currentQuestion < 1) {
+      setCurrentQuestion(currentQuestion + 1)
+      setRadioChecked(null)
     }
-    // let score = 0
-    // setResults(score)
-    setSubmitted(true)
+
   }
+  const handlePrevQuestion = () => {
+    if (currentQuestion > 0)
+      setCurrentQuestion(currentQuestion - 1)
+  }
+  const returnResults = () => {
+    const answers = props.quiz.map((question) => question.answer)
+    setSubmitted(true)
+
+    const updateResults = (index, answersArr) => {
+      userSelection.forEach(answer => answer === answersArr[index] ? setResults(+ 1) : null)
+    }
+    updateResults(answers)
+  }
+
+  // const handleSelection = () => {
+  //   setUserSelection()
+  //   console.log('user selection', userSelection)
+  // }
+  // onClick={() => setUserSelection([...userSelection, idx])}
+  //onClick={() => setUserSelection([userSelection, 'poo'])}
+
 
   return (
     <>
-      {!submitted && <div>{props.quiz[0].title}</div>}
-      {!submitted && <form>{props.quiz[0].options.map((option, idx) => <li>{option}<input name={option} onClick={() => setUserSelection(idx)} checked={userSelection === idx} style={{ marginLeft: "30px;" }} type="radio" value={idx}></input></li>)}</form>}
-      {/* <button onClick={handlePrevQuestion}>Last question</button> */}
-      <button disabled={userSelection === null}>Next question</button>
+      {!submitted && <div>{props.quiz[currentQuestion].title}</div>}
+
+      {!submitted && <form>{props.quiz[currentQuestion].options.map((option, idx) =>
+        <li>{option}<input name={option} value={idx} checked={radioChecked === idx} onClick={() => setRadioChecked(idx)} type="radio"></input></li>)}</form>}
+
+      <button onClick={handlePrevQuestion}>Last question</button>
+      <button onClick={handleNextQuestion} disabled={userSelection === null}>Next question</button>
       <button onClick={returnResults}>submit</button>
       {submitted && <div>`You scored {results} out of 1`</div>}
     </>
   )
 }
+
 
 export default Questions
